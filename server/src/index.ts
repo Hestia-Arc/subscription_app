cc0a079e9a75e3c9d702477dd7572c363e68326c
Afolabi Esther <starafolab@gmail.com>
2024-04-02 12:20:34 +0100

    add login  route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,26 @@
 import express from "express";
-import authRoutes from "./routes/auth"
+import authRoutes from "./routes/auth";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 // CREATE AN EXPRESS APP
 const app = express();
 
+dotenv.config();
+
 // CREATE A ROUTE
-app.use(express.json())
+app.use(express.json());
 app.use("/auth", authRoutes);
+
+// MONGOOSE CONNECTION
+mongoose
+  .connect(process.env.MONGO_URI as string) 
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    throw new Error(err);
+  });
 
 // LISTEN FOR REQUESTs
 app.listen("8000", () => {
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,14 +1,25 @@
 import express from "express";
 import { body, validationResult } from "express-validator";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
 
 const router = express.Router();
 
+// -----------------------------------
 // CREATE A POST REQUEST FOR SIGNING UP
+// ------------------------------------
 router.post(
   "/signup",
-  body("username").isString().not().isEmpty().withMessage("Username must be a string"),
+  body("username")
+    .isString()
+    .not()
+    .isEmpty()
+    .withMessage("Username must be a string"),
   body("email").isEmail().withMessage("Email is invalid"),
-  body("password").isLength({ min: 8 }).withMessage("Password is invalid, minimum of 8 characters"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password is invalid, minimum of 8 characters"),
   async (req, res) => {
     const validationErrors = validationResult(req);
 
@@ -22,11 +33,115 @@ router.post(
       return res.status(400).json({ errors });
     }
 
-    // IF NO ERROR,SEND THIS RESPONSE
-    res.json({
-      msg: "valid credentials",
+    // VERIFY EMAIL IN DB
+    const { username, email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.json({
+        errors: [
+          {
+            msg: "Email already exists",
+          },
+        ],
+        data: null,
+      });
+    }
+
+    // HASH PASSWORD
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+
+    // SAVE USER TO DB
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
     });
+
+    // CREATE AND SEND TOKEN
+    const token = await JWT.sign(
+      {email: newUser.email},
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: 360000
+      }
+    )
+
+    // SEND THE TOKEN
+    res.json({
+      errors: [],
+      data: {
+        token,
+        user: {
+          id: newUser._id,
+          email: newUser.email
+        }
+      },
+      msg: "Registration successful",
+    });
+
+    
   }
 );
+
+
+// ---------------------- 
+// POST REQUEST FOR LOGIN 
+// -----------------------
+router.post("/login", async (req, res) => {
+  const {email, password} = req.body;
+
+  const user = await User.findOne({email})
+
+  if(!user) {
+    return res.json({
+      errors: [{
+        msg: "Invalid email"
+      }],
+      data: null
+    })
+  }
+
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if(!isMatch) {
+    return res.json({
+      errors: [
+        {
+          msg: "Invalid credentials",
+        },
+      ],
+      data: null,
+    });
+  }
+
+    // CREATE  TOKEN
+    const token = await JWT.sign(
+      {email: user.email},
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: 360000
+      }
+    )
+
+    // SEND THE TOKEN
+    res.json({
+      errors: [],
+      data: {
+        token,
+        user: {
+          id: user._id,
+          email: user.email
+        }
+      },
+      msg: "login successful",
+    });
+
+})
+
+
 
 export default router;
